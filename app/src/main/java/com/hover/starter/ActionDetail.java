cc9663f5bf48cea8e0c0c39b5a9507fb30f3387c
ishuah <kariuki@ishuah.com>
2019-05-08 16:47:03 +0300

    back button on action detail activity

--- app/src/main/java/com/hover/starter/ActionDetail.java
+++ app/src/main/java/com/hover/starter/ActionDetail.java
@@ -1,10 +1,15 @@
 package com.hover.starter;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.hover.starter.ui.actions.ActionDetailFragment;
 
 public class ActionDetail extends AppCompatActivity {
@@ -27,5 +32,29 @@ public class ActionDetail extends AppCompatActivity {
                     .replace(R.id.action_detail_container, actionDetailFragment)
                     .commitNow();
         }
+        final Toolbar toolbar = findViewById(R.id.detail_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
+
+        final View titleLayout = findViewById(R.id.layout_title);
+        titleLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                CollapsingToolbarLayout.LayoutParams layoutParams = (CollapsingToolbarLayout.LayoutParams) toolbar.getLayoutParams();
+                layoutParams.height = titleLayout.getHeight() + getResources().getDimensionPixelSize(R.dimen.abc_action_bar_default_height_material);
+                toolbar.setLayoutParams(layoutParams);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            navigateUpTo(new Intent(this, MainActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
